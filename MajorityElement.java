88f2fff260e6f1564df312f4ae8928a3f404d476
B1sdsa <126801917+B1sdsa@users.noreply.github.com>
2023-06-14 10:18:34 +0530

    Create MajorityElement.java

---
+++ MajorityElement.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+class MajorityElement{
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        int mode=0,mc=0;
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=s.nextInt();
+        }
+        for(int i=0;i<n;i++){
+            int c=0;
+            for(int j=0;j<n;j++){
+                if (arr[j]==arr[i]){
+                    ++c;
+                }
+            }
+            if(c>mc){
+                mode=arr[i];
+                mc=c;
+            }
+        }
+        if(mode>n/2){
+            System.out.println(mode);
+        }
+        else{
+            System.out.println(-1);
+        }
+
+    }
+}
